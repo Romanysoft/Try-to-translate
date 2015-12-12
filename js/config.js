1885abdb0b70c6bae21d4270d82591fb41a56b9e
iansun <lauer3912@gmail.com>
2015-12-12 02:55:32 -0800

    第一次提交网站

---
+++ js/config.js
@@ -0,0 +1,7 @@
+(function () {
+    window.RTYConfig = {
+        appName:"Try to translate",
+        documentTitle:"Try to translate By Romanysoft",
+        googleUA:"UA-43238484-4"
+    }
+})();
---
+++ js/google-analytics.js
@@ -0,0 +1,16 @@
+(function() {
+
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+    $(document).ready(function() {
+        try {
+            var pageTracker = _gat._getTracker(window.RTYConfig.googleUA);
+            pageTracker._trackPageview();
+        } catch (err) {
+            console.error(err);
+        }
+    })
+
+
+})();
