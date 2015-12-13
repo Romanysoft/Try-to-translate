dd711a5e740e8b632e7c1381ec619773902cf9b4
iansun <lauer3912@gmail.com>
2015-12-12 22:11:26 -0800

    添加中文界面

--- js/config.js
+++ js/config.js
@@ -2,6 +2,9 @@
     window.RTYConfig = {
         appName:"Try to translate",
         documentTitle:"Try to translate By Romanysoft",
-        googleUA:"UA-43238484-7"
+        googleUA:"UA-43238484-7",
+        gitHome:"https://github.com/Romanysoft/Try-to-translate",
+        romanysoftHome:"http://www.romanysoft.com",
+        copyright:"Copyright © 2015 Romanysoft LAB."
     }
 })();
