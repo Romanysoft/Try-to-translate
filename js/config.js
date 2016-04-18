b267bcb91c505b65de0ab7eede63b8f69e194594
iansun <lauer3912@gmail.com>
2016-04-18 19:22:55 +0800

    更新网站

--- js/config.js
+++ js/config.js
@@ -3,10 +3,13 @@
         appName:"Try to translate",
         appID:"com.romanysoft.app.macos.TryToTranslate",
         documentTitle:"Try to translate By Romanysoft",
+        supportPlatforms:["Mac","Windows","Linux"],
         googleUA:"UA-43238484-7",
         gitHome:"https://github.com/Romanysoft/Try-to-translate",
         reportIssueUrl:"https://github.com/Romanysoft/Try-to-translate/issues",
-        romanysoftHome:"http://www.romanysoft.com",
-        copyright:"Copyright © 2016 Romanysoft LAB."
+        changeLogUrl:"https://github.com/Romanysoft/Try-to-translate/wiki/Changelog",
+        wikiUrl:"https://github.com/Romanysoft/Try-to-translate/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
     }
 })();
