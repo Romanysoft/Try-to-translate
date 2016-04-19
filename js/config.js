3aebfd91defbe5133ba8d948ca8b3c7e351b155e
iansun <lauer3912@gmail.com>
2016-04-19 21:06:56 +0800

    更新

--- js/config.js
+++ js/config.js
@@ -5,10 +5,10 @@
         documentTitle:"Try to translate By Romanysoft",
         supportPlatforms:["Mac","Windows","Linux"],
         googleUA:"UA-43238484-7",
-        gitHome:"https://github.com/Romanysoft/Try-to-translate",
-        reportIssueUrl:"https://github.com/Romanysoft/Try-to-translate/issues",
-        changeLogUrl:"https://github.com/Romanysoft/Try-to-translate/wiki/Changelog",
-        wikiUrl:"https://github.com/Romanysoft/Try-to-translate/wiki",
+        gitHome:"//github.com/Romanysoft/Try-to-translate",
+        reportIssueUrl:"//github.com/Romanysoft/Try-to-translate/issues",
+        changeLogUrl:"//github.com/Romanysoft/Try-to-translate/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/Try-to-translate/wiki",
         romanysoftHome:"//www.romanysoft.com",
         copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
     }
