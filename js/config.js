43e98974badf801f970bd7c52459960b87003b65
iansun <lauer3912@gmail.com>
2016-04-20 13:56:18 +0800

    使用新的google跟踪号

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
         appID:"com.romanysoft.app.macos.TryToTranslate",
         documentTitle:"Try to translate By Romanysoft",
         supportPlatforms:["Mac","Windows","Linux"],
-        googleUA:"UA-43238484-7",
+        googleUA:"UA-76676167-4",
         gitHome:"//github.com/Romanysoft/Try-to-translate",
         reportIssueUrl:"//github.com/Romanysoft/Try-to-translate/issues",
         changeLogUrl:"//github.com/Romanysoft/Try-to-translate/wiki/Changelog",
