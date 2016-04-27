46628154563c7690af36ae58048f305feceeeb8e
iansun <lauer3912@gmail.com>
2016-04-27 20:18:02 +0900

    TryToTranslate更新

--- js/views/view-releasenotes-page.js
+++ js/views/view-releasenotes-page.js
@@ -9,6 +9,13 @@
     c$ = $.extend(window.UI.c$, {});
     
     c$.updateReleasenotesPage = function(){
+        try{
+            if(window.location.href != RTYConfig.changeLogUrl){
+                window.location.href = RTYConfig.changeLogUrl;
+            }
+        }catch(e){
+            console.error(e);
+        }
         
         var l10n = c$.l10nFormatObj(c$.l10n["ReleaseNotePage"] || {});
         var htmlContent = "";
@@ -17,7 +24,7 @@
         htmlContent = template('changelog-content-tmpl', {labelVersion:l10n["labelVersion"] || "Version", list:l10n.list});
         $('.section-releasenotes').html(htmlContent);
         
-        window.location.href = RTYConfig.changeLogUrl;
+        
     };
     
     c$.MC_l10n.add(c$.updateReleasenotesPage);
